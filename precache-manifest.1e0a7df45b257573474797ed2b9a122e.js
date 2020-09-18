ea3b7c9e4e4d4147472f282bb6830c6f9c6d5960
vT <davidng9891@gmail.com>
2020-09-18 10:10:27 -0400

    Updates

---
+++ precache-manifest.1e0a7df45b257573474797ed2b9a122e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb40646c3cf1b1757586358fe5321fb",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1890704eef6cff75ffcc",
+    "url": "/react-monsters-rolodex/static/css/main.6e096feb.chunk.css"
+  },
+  {
+    "revision": "77263cff91b03fe4e343",
+    "url": "/react-monsters-rolodex/static/js/2.7b866b75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.7b866b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1890704eef6cff75ffcc",
+    "url": "/react-monsters-rolodex/static/js/main.bd1e1b24.chunk.js"
+  },
+  {
+    "revision": "d6358095527a558ee8fa",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.4417ba72.js"
+  }
+]);
